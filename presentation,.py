d5f07da439700bbd71dc17a22b5c26ff152402c0
ShearStress021 <shearstress021@gmail.com>
2024-10-13 12:04:02 -0700

    added more on presentation layer

--- presentation,.py
+++ presentation,.py
@@ -19,12 +19,26 @@ class BudgetTracker(ctk.CTk):
             "Summary",
             "Budget Analysis",
         ]
-        for tab_name in tabs_name:
-            self.tabs.add(tab_name)
-            # tabs.tab(tab_name)
 
-        for tab_name in tabs_name[:3]:
-            forms = InputForm(self.tabs.tab(tab_name))
+        tabs_dict_items = {
+            "Income": InputForm,
+            "Expenses": InputForm,
+            "Goals": InputForm,
+            "Visualization": VisualizationInputForm,
+            "Summary": SummaryInputForm,
+            "Budget Analysis": AnalysisInputForm,
+        }
+
+        # for tab_name in tabs_name:
+        #     self.tabs.add(tab_name)
+        #     # tabs.tab(tab_name)
+
+        # for i, tab_name in enumerate(tabs_name[:3]):
+        #     InputForm(self.tabs.tab(tab_name))
+
+        for tab_name, form_class in tabs_dict_items.items():
+            self.tabs.add(tab_name)
+            form_class(self.tabs.tab(tab_name), tab_name)
 
 
 class TabsCreate(ctk.CTkTabview):
@@ -34,7 +48,7 @@ class TabsCreate(ctk.CTkTabview):
 
 
 class InputForm(ctk.CTkFrame):
-    def __init__(self, parent) -> None:
+    def __init__(self, parent, button_label=None) -> None:
         super().__init__(parent)
 
         labels = ["Account Name:", "Category:", "Amount:", "Date (YYYY-MM-DD):"]
@@ -44,6 +58,59 @@ class InputForm(ctk.CTkFrame):
 
             entry = ctk.CTkEntry(self)
             entry.pack(pady=5)
+
+        button = ctk.CTkButton(self, text=f"Add {button_label}")
+        button.pack(pady=5)
+        self.pack(padx=10, pady=10, fill=ctk.BOTH, expand=True)
+
+
+class VisualizationInputForm(ctk.CTkFrame):
+    def __init__(self, parent, button_label=None) -> None:
+        super().__init__(parent)
+        self.pack(padx=10, pady=10, fill=ctk.BOTH, expand=True)
+
+        label = ctk.CTkLabel(self, text="Account Name")
+        label.pack(pady=5)
+
+        entry = ctk.CTkEntry(self)
+        entry.pack(pady=5)
+
+        button_texts = ["Bar", "Pie", "Line"]
+        for txt in button_texts:
+            button = ctk.CTkButton(self, text=f"Show {txt} Chart")
+            button.pack(pady=5)
+
+
+class SummaryInputForm(ctk.CTkFrame):
+    def __init__(self, parent, button_label=None):
+        super().__init__(parent)
+
+        label = ctk.CTkLabel(self, text="Account Name")
+        label.pack(pady=5)
+        entry = ctk.CTkEntry(self)
+        entry.pack(pady=5)
+
+        textbox = ctk.CTkTextbox(self, height=400, width=500)
+        textbox.pack(pady=10)
+
+        button = ctk.CTkButton(self, text="Display Summary")
+        button.pack(pady=10)
+        self.pack(padx=10, pady=10, fill=ctk.BOTH, expand=True)
+
+
+class AnalysisInputForm(ctk.CTkFrame):
+    def __init__(self, parent, button_label=None):
+        super().__init__(parent)
+
+        label = ctk.CTkLabel(self, text="Account Name")
+        label.pack(pady=5)
+        entry = ctk.CTkEntry(self)
+        entry.pack(pady=5)
+        textbox = ctk.CTkTextbox(self, height=400, width=500)
+        textbox.pack(pady=10)
+
+        button = ctk.CTkButton(self, text="Calculate Budget Analysis")
+        button.pack(pady=10)
         self.pack(padx=10, pady=10, fill=ctk.BOTH, expand=True)
 
 
